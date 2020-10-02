e31aa7e302aa38f17f2618ad7c483e48c0d37ce0
vancereyes <71995775+vancereyes@users.noreply.github.com>
2020-10-02 15:02:54 +0800

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,19 @@
+1.
+
+var user = {};
+user.name = "John";
+user.surname = "Mike";
+user.name = "Peter";
+delete user.name;
+
+2. 
+
+var fruit = {
+apple: 20,
+pear: 20,
+peach: 10
+};
+
+output = fruit.apple + fruit.pear + fruit.peach
+
+console.log(output);
